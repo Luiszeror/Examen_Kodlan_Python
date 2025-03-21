579737fc23dc6237ccd5068ebe8ae99baaeeed8c
Luis Esteban Robelto Z <117394702+Luiszeror@users.noreply.github.com>
2025-03-21 00:12:19 -0500

    Update Game.py
    Title changed

--- pythonGameKodland/Game.py
+++ pythonGameKodland/Game.py
@@ -7,7 +7,7 @@ pygame.init()
 # pantalla
 WIDTH, HEIGHT = 800, 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Defiende el Puente")
+pygame.display.set_caption("DEFIENDE EL PUENTE")
 
 # colores
 WHITE = (255, 255, 255)
